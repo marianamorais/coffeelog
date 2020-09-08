d6a7451821aa76851cee756fededa373a3c4a605
Mariana <moraismariana83@gmail.com>
2020-09-08 16:58:00 -0300

    seo

---
+++ scripts/map.js
@@ -0,0 +1,19 @@
+const mymap = L.map('map').setView([-23.56773, -46.64811], 17);
+const marker = L.marker([-23.56773, -46.64811]).addTo(mymap);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,20 @@
-const mymap = L.map('map').setView([-23.56773, -46.64811], 17);
-const marker = L.marker([-23.56773, -46.64811]).addTo(mymap);
+window.addEventListener('load', function(){
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    dots: '#dots',
+    draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+      }
+    })
+});
 
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(mymap);
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+if(window.SimpleForm) {
+  new SimpleForm({
+    form: ".formphp", // form selector
+    button: "#send", // button selector
+    error: "<div id='form-error'><h2>Error sending</h2><p>An error has occurred.</p></div>", // error message
+    success: "<div id='form-success'><h2>Successfully submitted form</h2><p>Soon we will contact you.</p></div>", // success message
+  });
 }
-
-mymap.on('click', onMapClick);
